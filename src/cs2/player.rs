806a999775f2f5d251544d4f5dfa3783d9b794ab
avitran0 <holyhades64@gmail.com>
2025-10-15 10:35:06 +0200

    fix entity list entry size

--- src/cs2/player.rs
+++ src/cs2/player.rs
@@ -60,7 +60,7 @@ impl Player {
             return None;
         }
         // what?
-        let entity = cs2.process.read(v1 + 120 * (index & 0x1ff));
+        let entity = cs2.process.read(v1 + 112 * (index & 0x1ff));
         if entity == 0 {
             return None;
         }
@@ -467,19 +467,19 @@ impl CS2 {
         self.weapon = local_player.weapon(self);
 
         self.players.clear();
-        
-        
+
+
         for i in 0..=64 {
             let player = match Player::index(self, i) {
                 Some(player) => player,
                 None => continue,
             };
-            
+
             if let Some(target) = player.spectator_target(self) {
                 let spectator_id = player.steam_id(self);
                 let target_pawn = target.pawn;
                 let local_pawn = local_player.pawn;
-                    
+
                 if target_pawn == local_pawn {
                     let spectator_name = player.name(self);
                     let local_steam_id = local_player.steam_id(self);
@@ -497,6 +497,6 @@ impl CS2 {
                 self.players.push(player);
             }
         }
-        
+
     }
 }
