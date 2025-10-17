9540a6ee2ec646f52dac11471d769d6334d2d6f5
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-10-17 22:18:37 +0300

    Update flake.nix
    Improve formatting and add comments.

--- flake.nix
+++ flake.nix
@@ -1,33 +1,36 @@
 {
   description = "deadlocked dev shell for nix users";
-
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default-linux";
   };
-
+  
   outputs = inputs @ {
     self,
     nixpkgs,
     systems,
     ...
   }: let
+    # Build outputs for each system
     eachSystem = nixpkgs.lib.genAttrs (import systems);
-
     pkgsFor = system:
       import nixpkgs {
         inherit system;
         overlays = [];
       };
   in {
+    # ValveResourceFormat CLI package
     packages = eachSystem (system: {
       source2viewer = nixpkgs.legacyPackages.${system}.callPackage ./nix/source2viewer.nix {};
     });
-
+    
+    # Development shell
     devShells = eachSystem (system: {
       default = (pkgsFor system).callPackage ./nix/shell.nix {};
     });
-
+    
+    # Code formatter
     formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
 }
