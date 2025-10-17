ba76edb6b78cb7e23ff35306295cbdbffa416006
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-10-17 22:33:51 +0300

    Refactor buildDotnetModule for ValveResourceFormat
    Refactor Nix expression for ValveResourceFormat build.

--- nix/source2viewer.nix
+++ nix/source2viewer.nix
@@ -1,22 +1,21 @@
-{ fetchFromGitHub
-, buildDotnetModule
-, dotnetCorePackages
-, ... }:
-buildDotnetModule rec {
-    pname = "ValveResourceFormat";
-    version = "15.0";
+{ fetchFromGitHub, buildDotnetModule, dotnetCorePackages }:
 
-    src = fetchFromGitHub {
-      owner = pname;
-      repo = pname;
-      rev = "aa1c384b9481a6892b3963d96251b3065191db47";
-      sha256 = "sha256-1p7AeiZ5Bgre77XuS5xjwjk4Kzs0spEh2e/WHLuQG5I=";
-    };
+# Builds ValveResourceFormat CLI from source
+buildDotnetModule {
+  pname = "ValveResourceFormat";
+  version = "15.0";
 
-    nugetDeps = ./deps.json;
+  # GitHub source
+  src = fetchFromGitHub {
+    owner = "ValveResourceFormat";
+    repo = "ValveResourceFormat";
+    rev = "aa1c384b9481a6892b3963d96251b3065191db47";
+    sha256 = "sha256-1p7AeiZ5Bgre77XuS5xjwjk4Kzs0spEh2e/WHLuQG5I=";
+  };
 
+  nugetDeps = ./deps.json;
+  # .NET 9.0 runtime and SDK
   dotnet-sdk = dotnetCorePackages.sdk_9_0;
   dotnet-runtime = dotnetCorePackages.runtime_9_0;
-
   projectFile = "./CLI/CLI.csproj";
 }
