5c86d6b07e15c6ac1eaf87cd7aa329198a047fc4
avitran0 <holyhades64@gmail.com>
2025-10-18 19:33:46 +0200

    fix rebuilding every time

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use copy_to_output::copy_to_output;
 
 fn main() {
-    println!("cargo:rerun-if-changed=resources/source2viewer/*");
-    copy_to_output("resources/source2viewer", &std::env::var("PROFILE").unwrap()).unwrap();
+    println!("cargo:rerun-if-changed=resources/source2viewer/Source2Viewer-CLI");
+    let _ = copy_to_output("resources/source2viewer", &std::env::var("PROFILE").unwrap());
 }
