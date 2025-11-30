0d04dfc467b7b7413f69c46092afcf25e3b882ea
id3v1669 <id3v1669@gmail.com>
2025-11-30 12:42:43 +0800

    chore: bump flake & source2viewer

--- nix/source2viewer.nix
+++ nix/source2viewer.nix
@@ -4,21 +4,21 @@
   dotnetCorePackages,
 }:
 # Builds ValveResourceFormat CLI from source
-buildDotnetModule {
+buildDotnetModule rec {
   pname = "ValveResourceFormat";
-  version = "15.0";
+  version = "16.0";
 
   # GitHub source
   src = fetchFromGitHub {
     owner = "ValveResourceFormat";
     repo = "ValveResourceFormat";
-    rev = "aa1c384b9481a6892b3963d96251b3065191db47";
-    sha256 = "sha256-1p7AeiZ5Bgre77XuS5xjwjk4Kzs0spEh2e/WHLuQG5I=";
+    rev = "${version}";
+    sha256 = "sha256-eFGCS0mS27z+3ffZ2no8XQ6znEm71vO9kR99SReVFdg=";
   };
 
   nugetDeps = ./deps.json;
-  # .NET 9.0 runtime and SDK
-  dotnet-sdk = dotnetCorePackages.sdk_9_0;
-  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+  # .NET 10.0 runtime and SDK
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = dotnetCorePackages.runtime_10_0;
   projectFile = "./CLI/CLI.csproj";
 }
