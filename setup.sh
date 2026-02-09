b58066e21ece68f3719a66b57af02970456910a8
avitran0 <holyhades64@gmail.com>
2026-02-09 14:29:28 +0100

    add setup log

--- setup.sh
+++ setup.sh
@@ -5,12 +5,16 @@ UINPUT_GROUP="uinput"
 CURRENT_USER=$(whoami)
 
 echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput"' | sudo tee "$UDEV_RULE_FILE" > /dev/null
+echo "created udev file: $UDEV_RULE_FILE"
 
 if ! getent group "$UINPUT_GROUP" > /dev/null; then
     sudo groupadd "$UINPUT_GROUP"
+    echo "created group $UINPUT_GROUP"
 fi
 
-sudo usermod -aG "$UINPUT_GROUP" $(whoami)
+sudo usermod -aG "$UINPUT_GROUP" "$CURRENT_USER"
+echo "added user $CURRENT_USER to group $UINPUT_GROUP"
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+echo "reloaded udev rules"
